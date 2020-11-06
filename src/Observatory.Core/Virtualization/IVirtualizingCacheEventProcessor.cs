5a4a04f4d84f7c52b309320717eac94383f2641b
Nguyen Duy Hung <hungnd1475@outlook.com>
2020-11-06 15:09:48 +1100

    Spelling

--- src/Observatory.Core/Virtualization/IVirtualizingCacheEventProcessor.cs
+++ src/Observatory.Core/Virtualization/IVirtualizingCacheEventProcessor.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Observatory.Core.Virtualization
 {
     /// <summary>
-    /// Defines a contract for implementing a event processor that processes instances of <see cref="IVirtualizingCacheEvent{T}"/>.
+    /// Defines a contract for implementing an event processor that processes instances of <see cref="IVirtualizingCacheEvent{T}"/>.
     /// </summary>
     /// <typeparam name="T">The type of the items held by the cache that produces instances of <see cref="IVirtualizingCacheEvent{T}"/>.</typeparam>
     /// <typeparam name="R">The type of result returned after being processed.</typeparam>
